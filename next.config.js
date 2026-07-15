c86f8e2d39ce41bbd4210742f9c210f685b38815
yansihanevych-bit <yansihanevych@gmail.com>
2026-07-15 04:50:18 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   // ✅ Enable compression
   compress: true,
@@ -96,4 +99,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
